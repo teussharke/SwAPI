03d644837788430048bcec36318767557912d4c1
Mateus Oliveira <teussharke@gmail.com>
2023-01-09 17:54:18 -0300

    att

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
-import { useState, useEffect } from 'react'
-import './App.css'
-import { People } from './pages/Peoples'
+import { useState, useEffect } from "react";
+import "./App.css";
 
-const url = "https://swapi.dev/api/people/1/"
+const url = "https://swapi.dev/api/people/1/";
 
- export function App() {
-    const [people, setPeople] = useState([]);
- 
 
-    // 1 - resgatando dados
-    useEffect(() => {
-      async function fetchData() {
+export function App() {
+  const [people, setPeople] = useState([]);
+  const [name, setName] = useState("");
+  const [height, setHeight] = useState(0);
+  const [mass, setMass] = useState(0);
+  const [gender, setGender] = useState("");
+  const [films, setFilms] = useState("");
+
+  // 1 - resgatando dados
+  useEffect(() => {
+    async function fetchData() {
       const res = await fetch(url);
 
       const data = await res.json();
@@ -18,9 +22,9 @@ const url = "https://swapi.dev/api/people/1/"
       setPeople(data);
     }
     fetchData();
-    }, []);
+  }, []);
 
-    console.log(people);
-    
+  console.log(people);
 
- }
+  return <div className="App"></div>;
+}
